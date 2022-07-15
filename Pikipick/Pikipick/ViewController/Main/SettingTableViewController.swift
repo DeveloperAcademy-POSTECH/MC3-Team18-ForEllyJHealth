e348c1925cb6aab0ede57956cbaeab929091f473
woo0 <57060443+woo0dev@users.noreply.github.com>
2022-07-15 21:22:50 +0900

    기본 UI 구성

---
+++ Pikipick/Pikipick/ViewController/Main/SettingTableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SettingTableViewController.swift
+//  Pikipick
+//
+//  Created by woo0 on 2022/07/15.
+//
+
+import UIKit
+
+class SettingTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+		
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 0
+    }
+
+
+}
