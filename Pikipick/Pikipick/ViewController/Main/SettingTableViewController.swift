99b53858ec52f45e53bcdc877d22e6548f13d02a
woo0 <57060443+woo0dev@users.noreply.github.com>
2022-07-16 17:35:08 +0900

    SettingTableViewController 구현

--- Pikipick/Pikipick/ViewController/Main/SettingTableViewController.swift
+++ Pikipick/Pikipick/ViewController/Main/SettingTableViewController.swift
@@ -8,22 +8,31 @@
 import UIKit
 
 class SettingTableViewController: UITableViewController {
+	var SettingMenus = ["오픈소스 라이센스", "버전정보"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
 		
     }
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        
-        return 0
-    }
-
+	
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return 0
+        return 2
     }
+	
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+		cell.textLabel?.text = SettingMenus[indexPath.row]
+		
+		return cell
+	}
+	
+	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		if indexPath.row == 0 {
+			guard let vc = storyboard?.instantiateViewController(withIdentifier: "LicenseVC") as? LicenseViewController else { return }
+			navigationController?.pushViewController(vc, animated: true)
+		}
+	}
 
 
 }
