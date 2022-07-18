96d6b50cf7cc5c874bcabc022a5cf8e3e022f2d6
woo0 <57060443+woo0dev@users.noreply.github.com>
2022-07-18 17:07:28 +0900

    Update LicenseViewController.swift

--- Pikipick/Pikipick/ViewController/Main/LicenseViewController.swift
+++ Pikipick/Pikipick/ViewController/Main/LicenseViewController.swift
@@ -8,13 +8,9 @@
 import UIKit
 
 class LicenseViewController: UIViewController {
-	var openSources: [OpenSource] = [OpenSource(openSourceName: "Apple Frameworks", licenseName: "Apple MIT License", url: "https://developer.apple.com/documentation/", copyright: "Copyright Apple lnc."), OpenSource(openSourceName: "Apple Swift", licenseName: "Apache-2.0 license", url: "https://github.com/apple/swift", copyright: "Copyright Apple lnc. and the Swift project authors")]
-	var licenses: [License] = [License(licenseName: "Apache-2.0 license", licanseNotice: "ApacheLicense.txt"), License(licenseName: "MIT License", licanseNotice: "MITLicense.txt")]
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
     }
     
 }
