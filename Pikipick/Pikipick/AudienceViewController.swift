fd879892687bd75378803b4a0f6e43832f20fc52
dkdo1406 <54826394+dkdo1406@users.noreply.github.com>
2022-07-19 15:53:31 +0900

    viewFadeOut함수 추가

--- Pikipick/Pikipick/AudienceViewController.swift
+++ Pikipick/Pikipick/AudienceViewController.swift
@@ -19,47 +19,35 @@ class AudienceViewController: UIViewController {
     
     @IBAction func tabClapButton(_ sender: UIButton) {
         self.emoji.text = "👏"
-        UIView.animate(withDuration: 5, animations: {
-            self.emoji.alpha = 1
-            self.emoji.alpha = 0
-        })
+        viewFadeOut()
     }
     
     @IBAction func tabSurpriseButton(_ sender: UIButton) {
         self.emoji.text = "😮"
-        UIView.animate(withDuration: 5, animations: {
-            self.emoji.alpha = 1
-            self.emoji.alpha = 0
-        })
+        viewFadeOut()
     }
     
     @IBAction func tapCelebrateButton(_ sender: UIButton) {
         self.emoji.text = "🎉"
-        UIView.animate(withDuration: 5, animations: {
-            self.emoji.alpha = 1
-            self.emoji.alpha = 0
-        })
-
+        viewFadeOut()
     }
     
     @IBAction func tapFireButton(_ sender: UIButton) {
         self.emoji.text = "🔥"
-        UIView.animate(withDuration: 5, animations: {
-            self.emoji.alpha = 1
-            self.emoji.alpha = 0
-        })
+        viewFadeOut()
     }
     
     @IBAction func tapCuriousbutton(_ sender: UIButton) {
         self.emoji.text = "🤔"
-        UIView.animate(withDuration: 5, animations: {
-            self.emoji.alpha = 1
-            self.emoji.alpha = 0
-        })
+        viewFadeOut()
     }
     
     @IBAction func tapGoodButton(_ sender: UIButton) {
         self.emoji.text = "👍"
+        viewFadeOut()
+    }
+    
+    func viewFadeOut() {
         UIView.animate(withDuration: 5, animations: {
             self.emoji.alpha = 1
             self.emoji.alpha = 0
