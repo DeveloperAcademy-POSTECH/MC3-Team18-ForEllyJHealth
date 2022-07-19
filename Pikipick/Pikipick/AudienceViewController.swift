4a85b8b037752e42bb8c8e5f050c1db0e47ddd11
jeonghyeon Hwang <hijihilin@gmail.com>
2022-07-20 05:24:05 +0900

    [FEAT] SessionAudience 연결 (#21)

--- Pikipick/Pikipick/AudienceViewController.swift
+++ Pikipick/Pikipick/AudienceViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import MultipeerConnectivity
 
 class AudienceViewController: UIViewController {
 
@@ -13,38 +14,55 @@ class AudienceViewController: UIViewController {
 	@IBOutlet weak var titleLabel: UILabel!
 	@IBOutlet weak var emoji: UILabel!
 	
-	var deviceName: String?
+	var deviceName: MCPeerID?
+    private var audience = SessionAudience()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-		if let text = deviceName {
-			titleLabel.text = "\(text)의 발표입니다."
+        
+        let deviceNameLength = (deviceName?.displayName.count ?? minusPresenterSuffixNum) - minusPresenterSuffixNum
+        if let text = deviceName?.displayName {
+            titleLabel.text = "\(text.substring(from: 0, to: deviceNameLength))의 발표입니다."
 		}
+        
+        audience.startAdvertise()
+        audience.currentPresenter = deviceName
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        audience.stopAdvertise()
+        audience.sessionDisconnect()
     }
     
     @IBAction func tabClapButton(_ sender: UIButton) {
         emoji.text = "👏"
         viewFadeOut(fadeOutTime: fadeOutTime)
+        audience.send(emoji: EmojiName.Marvelous, receiver: deviceName!)
     }
     @IBAction func tabSurpriseButton(_ sender: UIButton) {
         emoji.text = "😮"
         viewFadeOut(fadeOutTime: fadeOutTime)
+        audience.send(emoji: EmojiName.Surprising, receiver: deviceName!)
     }
     @IBAction func tapCelebrateButton(_ sender: UIButton) {
         emoji.text = "🎉"
         viewFadeOut(fadeOutTime: fadeOutTime)
+        audience.send(emoji: EmojiName.Congrats, receiver: deviceName!)
     }
     @IBAction func tapFireButton(_ sender: UIButton) {
         emoji.text = "🔥"
         viewFadeOut(fadeOutTime: fadeOutTime)
+        audience.send(emoji: EmojiName.LEGO, receiver: deviceName!)
     }
     @IBAction func tapCuriousbutton(_ sender: UIButton) {
         emoji.text = "🤔"
         viewFadeOut(fadeOutTime: fadeOutTime)
+        audience.send(emoji: EmojiName.Idk, receiver: deviceName!)
     }
     @IBAction func tapGoodButton(_ sender: UIButton) {
         emoji.text = "👍"
         viewFadeOut(fadeOutTime: fadeOutTime)
+        audience.send(emoji: EmojiName.Good, receiver: deviceName!)
     }
 
     func viewFadeOut(fadeOutTime: Int) {
