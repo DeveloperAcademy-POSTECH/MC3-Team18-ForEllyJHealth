0bfcecac9efab0a98ec1331af1b10384db6c7ed2
song-cool <oscar2978@naver.com>
2022-07-20 19:31:49 +0900

    [FEAT] 프리젠터 연결
    TODO
    -[ ] 컴바인으로 프리젠터의 내부 데이터를 통해서 섭스크라이버에서 버튼의 액션들을 호출해주어야함

--- Pikipick/Pikipick/ButtonActionView.swift
+++ Pikipick/Pikipick/ButtonActionView.swift
@@ -5,6 +5,7 @@
 //  Created by Jun.Mac on 2022/07/20.
 //
 import SwiftUI
+import Combine
 
 struct Emoji: Hashable {
     var emoticon: String
@@ -12,6 +13,22 @@ struct Emoji: Hashable {
 }
 
 struct ButtonActionView: View {
+    
+    
+    @StateObject var presenter = SessionPresenter()
+    // 컴바인을 어찌하고싶은지 확인하는것
+    // 참고자료.. 확인해보세요..
+    //https://zeddios.tistory.com/925
+    // 출처: https://zeddios.tistory.com/925 [ZeddiOS:티스토리]
+    //    let subscriber = presenter.sink { (value) in
+    //        print(value)
+    //    }
+    //        .onChange(of: SessionPresenter.currentEmoji?.rawValue) { value in
+    //            print(value)
+    //        }
+    
+    @State var isPresentationReady : Bool = false
+    //컴바인 섭스크라이브를 참고
     @State private var counter: Int = 0
     @State var emoji = [
         Emoji(emoticon: "👏"),
@@ -25,8 +42,20 @@ struct ButtonActionView: View {
     var body: some View {
         HStack {
             ForEach(0 ..< emoji.count, id:\.self){ i in
-            ButtonAnimView(emoji: $emoji[i])
+                ButtonAnimView(emoji: $emoji[i])
             }
+        }
+        .onAppear(){
+            print("BROWSING START!")
+            presenter.startBrowsing()
+            presenter.startAdvertise()
+            //            presenter.publisher(for: <#T##KeyPath<Self, Value>#>, options: <#T##Foundation.NSKeyValueObservingOptions#>)
+        }
+        .onDisappear(){
+            print("BROWSING STOP!")
+            presenter.stopAdvertise()
+            presenter.stopBrowsing()
+            presenter.sessionDisconnect()
         }
     }
 }
@@ -36,3 +65,4 @@ struct ButtonActionView_Previews: PreviewProvider {
         ButtonActionView()
     }
 }
+
