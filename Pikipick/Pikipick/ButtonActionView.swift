d3838a420a5ed8c8369e30ce1e7d159edd50b783
jeonghyeon Hwang <hijihilin@gmail.com>
2022-07-20 22:27:50 +0900

    [STYLE] 필요없는 Comment 제거 (#21)

--- Pikipick/Pikipick/ButtonActionView.swift
+++ Pikipick/Pikipick/ButtonActionView.swift
@@ -14,23 +14,9 @@ struct Emoji: Hashable {
 
 struct ButtonActionView: View {
     
-    
     @StateObject var presenter = SessionPresenter()
+    
     @State var currentEmoji: String = ""
-    // 컴바인을 어찌하고싶은지 확인하는것
-    // 참고자료.. 확인해보세요..
-    //https://zeddios.tistory.com/925
-    // 출처: https://zeddios.tistory.com/925 [ZeddiOS:티스토리]
-    //    let subscriber = presenter.sink { (value) in
-    //        print(value)
-    //    }
-    //        .onChange(of: SessionPresenter.currentEmoji?.rawValue) { value in
-    //            print(value)
-    //        }
-    
-    @State var isPresentationReady : Bool = false
-    //컴바인 섭스크라이브를 참고
-    
     @State private var counter: Int = 0
     @State var emoji = [
         Emoji(emoticon: "👏"),
@@ -44,14 +30,13 @@ struct ButtonActionView: View {
     var body: some View {
         HStack {
             ForEach(0 ..< emoji.count, id:\.self){ i in
-                ButtonAnimView(emoji: $emoji[i], presenter: presenter)
+                ButtonAnimView(presenter: presenter, emoji: $emoji[i])
             }
         }
         .onAppear(){
             print("BROWSING START!")
             presenter.startBrowsing()
             presenter.startAdvertise()
-            //            presenter.publisher(for: <#T##KeyPath<Self, Value>#>, options: <#T##Foundation.NSKeyValueObservingOptions#>)
         }
         .onDisappear(){
             print("BROWSING STOP!")
@@ -62,8 +47,8 @@ struct ButtonActionView: View {
     }
 }
 
-//struct ButtonActionView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ButtonActionView()
-//    }
-//}
+struct ButtonActionView_Previews: PreviewProvider {
+    static var previews: some View {
+        ButtonActionView()
+    }
+}
