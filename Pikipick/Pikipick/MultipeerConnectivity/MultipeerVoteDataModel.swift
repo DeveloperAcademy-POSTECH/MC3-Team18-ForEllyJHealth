f4e160c54c83a215aff61138e8af9f8875b23267
jeonghyeon Hwang <hijihilin@gmail.com>
2022-07-25 23:28:03 +0900

    [FEAT] 투표 케이스 enum 추가 (#43)

---
+++ Pikipick/Pikipick/MultipeerConnectivity/MultipeerVoteDataModel.swift
@@ -0,0 +1,12 @@
+//
+//  MultipeerVoteDataModel.swift
+//  Pikipick
+//
+//  Created by 황정현 on 2022/07/25.
+//
+
+import Foundation
+
+enum Vote: Int, CaseIterable {
+    case yes, no, option1, option2, option3, option4
+}
