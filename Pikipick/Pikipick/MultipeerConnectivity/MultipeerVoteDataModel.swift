e720255c402f7059974cc2d42d0033fd847522ac
jeonghyeon Hwang <hijihilin@gmail.com>
2022-07-26 01:19:59 +0900

    [FEAT] 투표 String -> Dictionary 변환 메소드 추가 (#43)

--- Pikipick/Pikipick/MultipeerConnectivity/MultipeerVoteDataModel.swift
+++ Pikipick/Pikipick/MultipeerConnectivity/MultipeerVoteDataModel.swift
@@ -10,3 +10,15 @@ import Foundation
 enum Vote: Int, CaseIterable {
     case yes, no, option1, option2, option3, option4
 }
+
+func voteIs (data: String) -> [String: Int]? {
+    guard let strIndex = data.firstIndex(of: "|") else { return ["NIL PEER": -1]}
+    let peerNameLastIndex = data.distance(from: data.startIndex, to: strIndex)
+    let peerName = data.substring(from: 0, to: peerNameLastIndex)
+    let voteResult = data.substring(from: peerNameLastIndex+1, to: data.count - 1)
+    if let vote = Int(voteResult) {
+        return [peerName: vote]
+    } else {
+        return [peerName: -1]
+    }
+}
