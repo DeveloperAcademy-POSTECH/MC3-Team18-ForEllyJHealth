316c85cd1d4780cc01bd6e0b83f24e76dc25db0c
jeonghyeon Hwang <hijihilin@gmail.com>
2022-07-27 01:12:56 +0900

    [FEAT] Property 추가 (#52)

--- Pikipick/Pikipick/PresentationMenuButtonView.swift
+++ Pikipick/Pikipick/PresentationMenuButtonView.swift
@@ -8,27 +8,38 @@
 import SwiftUI
 
 // https://swifttom.com/2020/10/23/how-to-make-a-expandable-button-in-swiftui/
-struct MenuButtons: View {
+struct MenuButton: View {
     var buttonName: String
-    var buttonColor: Color
+    var buttonTextColor: Color
     
+    private let fontSize = CGFloat(12)
     var body: some View {
         Button(action: {
             print("Menu Button tapped")
             
         }) {
             Text(buttonName)
-                .font(.system(size: 12))
-                .foregroundColor(buttonColor)
+                .font(.system(size: fontSize))
+                .foregroundColor(buttonTextColor)
         }
         .frame(width:64, height: 24)
     }
 }
 
 struct PresentationMenuButtonView: View {
-    var deviceName: String
     @State var isExpanded = false
     @State private var scale = 1.0
+    
+    var deviceName: String
+    private let buttonWH = CGFloat(32)
+    private let textHorPadding = CGFloat(16)
+    private let viewHorPadding = CGFloat(24)
+    private let deviceNameWidth = CGFloat(166)
+    private let defaultTextColor = Color.white
+    private let closeButtonColor = Color.closeButtonColor
+    private let bgNonExpandedFrameWidth = CGFloat(230)
+    private let bgExpandedFrameWidth = CGFloat(422)
+    private let bgHeight = CGFloat(40)
     
     var body: some View {
         VStack(spacing: 0) {
@@ -37,7 +48,7 @@ struct PresentationMenuButtonView: View {
                     Spacer()
                     RoundedRectangle(cornerRadius: 10)
                         .fill(Color.secondaryColor)
-                        .frame(width: isExpanded ? 422 : 230, height: 40, alignment: .trailing)
+                        .frame(width: isExpanded ? bgExpandedFrameWidth : bgNonExpandedFrameWidth, height: bgHeight, alignment: .trailing)
                         .animation(.easeInOut(duration: 0.1), value: isExpanded)
                 }
                 HStack(spacing: 0) {
@@ -50,36 +61,36 @@ struct PresentationMenuButtonView: View {
                         }) {
                             Image(isExpanded ? "icn_chevron_right_32px" : "icn_chevron_left_32px")
                                 .resizable()
-                                .frame(width: 32, height: 32)
+                                .frame(width: buttonWH, height: buttonWH)
                         }.animation(.spring(), value: scale)
                             
                         if isExpanded {
                             Divider()
-                                .frame(height: 32)
+                                .frame(height: buttonWH)
                                 .overlay(Color.dividerColor)
-                            MenuButtons(buttonName: "CLOSE", buttonColor: Color.closeButtonColor)
+                            MenuButton(buttonName: "CLOSE", buttonTextColor: closeButtonColor)
                             Divider()
-                                .frame(height: 32)
+                                .frame(height: buttonWH)
                                 .overlay(Color.dividerColor)
-                            MenuButtons(buttonName: "VOTE", buttonColor: .white)
+                            MenuButton(buttonName: "VOTE", buttonTextColor: defaultTextColor)
                             Divider()
-                                .frame(height: 32)
+                                .frame(height: buttonWH)
                                 .overlay(Color.dividerColor)
-                            MenuButtons(buttonName: "Q&A", buttonColor: .white)
+                            MenuButton(buttonName: "Q&A", buttonTextColor: defaultTextColor)
                         }
                         Divider()
-                            .frame(height: 32)
+                            .frame(height: buttonWH)
                             .overlay(Color.dividerColor)
                         Text(deviceName)
-                            .frame(width: 166)
+                            .frame(width: deviceNameWidth)
                             .foregroundColor(.white)
                             .font(.system(size: 17))
-                            .padding([.horizontal], 16)
+                            .padding([.horizontal], textHorPadding)
                     }
                     
                 }
             }
-            .padding([.top], 24)
+            .padding([.top], viewHorPadding)
             Spacer()
         }
         .background(.black)
