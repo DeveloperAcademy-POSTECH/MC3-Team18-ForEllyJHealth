6fa31e5981bedb01ea39d7550c2045bbbb630074
dkdo1406 <54826394+dkdo1406@users.noreply.github.com>
2022-07-28 08:11:05 +0900

    [FEAT] segment control추가

--- Pikipick/Pikipick/AudienceViewController.swift
+++ Pikipick/Pikipick/AudienceViewController.swift
@@ -9,27 +9,62 @@ import MultipeerConnectivity
 import UIKit
 
 class AudienceViewController: UIViewController {
-
+    
     private let fadeOutTime = 3
     var isHapticOn : Bool = true
     var impactFeedbackGenerator: UIImpactFeedbackGenerator?
     
-	@IBOutlet weak var titleLabel: UILabel!
-	@IBOutlet weak var emoji: UILabel!
-	
-	var deviceName: MCPeerID?
-    private var audience = SessionAudience()
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var emojiView: UIView!
+    @IBOutlet weak var voteView: UIView!
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    @IBOutlet weak var clapButton: UIButton!
+    @IBOutlet weak var surpriseButton: UIButton!
+    @IBOutlet weak var celebrateButton: UIButton!
+    @IBOutlet weak var fireButton: UIButton!
+    @IBOutlet weak var curiousButton: UIButton!
+    @IBOutlet weak var goodButton: UIButton!
+    @IBOutlet weak var yesButton: UIButton!
+    @IBOutlet weak var noButton: UIButton!
+    @IBOutlet weak var oneButton: UIButton!
+    @IBOutlet weak var twoButton: UIButton!
+    @IBOutlet weak var threeButton: UIButton!
+    @IBOutlet weak var fourButton: UIButton!
+    @IBOutlet weak var longPressGesture: UILongPressGestureRecognizer!
     
+    @IBOutlet weak var sendButton: UIButton!
+    var beforeButton: UIButton!
+    
+    var emoji: String = ""
+    var emoji123: UILabel!
+//    var emoji123: String = ""
+    
+    var deviceName: MCPeerID?
+    private var audience = SessionAudience()
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         let deviceNameLength = (deviceName?.displayName.count ?? minusPresenterSuffixNum) - minusPresenterSuffixNum
         if let text = deviceName?.displayName {
-            titleLabel.text = "\(text.substring(from: 0, to: deviceNameLength))의 발표입니다."
-		}
+            titleLabel.text = "\(text.substring(from: 0, to: deviceNameLength))"
+        }
+        
+        segmentControl?.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.white], for: UIControl.State.normal)
+        segmentControl?.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.black], for: UIControl.State.selected)
         
         audience.startAdvertise()
         audience.currentPresenter = deviceName
+    }
+    
+    
+    
+    @IBAction func switchViews(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            emojiView.alpha = 1.0
+            voteView?.alpha = 0.0
+        } else {
+            emojiView.alpha = 0.0
+            voteView?.alpha = 1.0
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -45,51 +80,116 @@ class AudienceViewController: UIViewController {
         audience.sessionDisconnect()
     }
     
+    @IBAction func sendIt(_ sender: UILongPressGestureRecognizer) {
+        if longPressGesture.state.rawValue == 1 {
+            print(emoji)
+            print(emoji123.text)
+            print("완료됐음")
+            audience.sendEmoji(sendEmoji: emoji, receiver: deviceName!)
+        }
+    }
+    
+    
     @IBAction func tabClapButton(_ sender: UIButton) {
-        emoji.text = "👏"
-        viewFadeOut(fadeOutTime: fadeOutTime)
-        audience.sendEmoji(sendEmoji: (emoji.text ?? ""), receiver: deviceName!)
+        print(emoji)
+        emoji123.text = "👏"
+        selectemoji(button: clapButton, beforeButton: beforeButton)
+        emoji = "👏"
         runHaptic(isHapticOn: isHapticOn)
+        print(emoji123.text!)
     }
     @IBAction func tabSurpriseButton(_ sender: UIButton) {
-        emoji.text = "😮"
-        viewFadeOut(fadeOutTime: fadeOutTime)
-        audience.sendEmoji(sendEmoji: (emoji.text ?? ""), receiver: deviceName!)
+        selectemoji(button: surpriseButton, beforeButton: beforeButton)
+        emoji = "😮"
+        emoji123?.text = "😮"
         runHaptic(isHapticOn: isHapticOn)
     }
     @IBAction func tapCelebrateButton(_ sender: UIButton) {
-        emoji.text = "🎉"
-        viewFadeOut(fadeOutTime: fadeOutTime)
-        audience.sendEmoji(sendEmoji: (emoji.text ?? ""), receiver: deviceName!)
+        selectemoji(button: celebrateButton, beforeButton: beforeButton)
+        emoji = "🎉"
         runHaptic(isHapticOn: isHapticOn)
     }
     @IBAction func tapFireButton(_ sender: UIButton) {
-        emoji.text = "🔥"
-        viewFadeOut(fadeOutTime: fadeOutTime)
-        audience.sendEmoji(sendEmoji: (emoji.text ?? ""), receiver: deviceName!)
+        selectemoji(button: fireButton, beforeButton: beforeButton)
+        emoji = "🔥"
         runHaptic(isHapticOn: isHapticOn)
     }
     @IBAction func tapCuriousbutton(_ sender: UIButton) {
-        emoji.text = "🤔"
-        viewFadeOut(fadeOutTime: fadeOutTime)
-        audience.sendEmoji(sendEmoji: (emoji.text ?? ""), receiver: deviceName!)
+        selectemoji(button: curiousButton, beforeButton: beforeButton)
+        emoji = "🤔"
         runHaptic(isHapticOn: isHapticOn)
     }
     @IBAction func tapGoodButton(_ sender: UIButton) {
-        emoji.text = "👍"
-        viewFadeOut(fadeOutTime: fadeOutTime)
-        audience.sendEmoji(sendEmoji: (emoji.text ?? ""), receiver: deviceName!)
+        selectemoji(button: goodButton, beforeButton: beforeButton)
+        emoji = "👍"
         runHaptic(isHapticOn: isHapticOn)
     }
-
-    func viewFadeOut(fadeOutTime: Int) {
-        UIView.animate(withDuration: TimeInterval(fadeOutTime), animations: {
-            self.emoji.alpha = 1
-            self.emoji.alpha = 0
-        })
+    
+    @IBAction func tapYesButton(_ sender: UIButton) {
+        selectemoji(button: yesButton, beforeButton: beforeButton)
+        emoji = "🙆‍♂️"
     }
     
-    func runHaptic(isHapticOn: Bool){
+    @IBAction func tapNoButton(_ sender: UIButton) {
+        selectemoji(button: noButton, beforeButton: beforeButton)
+        emoji = "🙅‍♀️"
+    }
+    
+    @IBAction func tapOneButton(_ sender: UIButton) {
+        selectemoji(button: oneButton, beforeButton: beforeButton)
+        emoji = "1️⃣"
+    }
+    
+    @IBAction func tapTwoButton(_ sender: UIButton) {
+        selectemoji(button: twoButton, beforeButton: beforeButton)
+        emoji = "2️⃣"
+    }
+    
+    @IBAction func tapThreeButton(_ sender: UIButton) {
+        selectemoji(button: threeButton, beforeButton: beforeButton)
+        emoji = "3️⃣"
+    }
+    
+    @IBAction func tapFourButton(_ sender: UIButton) {
+        selectemoji(button: fourButton, beforeButton: beforeButton)
+        emoji = "4️⃣"
+    }
+    
+    
+    @IBAction func pressButton(_ sender: UIButton) {
+        print(emoji)
+        print("시작")
+        sendButton?.configuration?.background.strokeColor = .yellow
+        sendButton?.configuration?.baseBackgroundColor = UIColor(named: "primaryColor")
+        
+        //        UIColor(named: "primaryColor") as? CGColor
+        //        sendButton.configuration.stock = UIColor
+    }
+    
+    @IBAction func pressAction(_ sender: UILongPressGestureRecognizer) {
+        //        print(emoji)
+        print(longPressGesture.state.rawValue)
+        if longPressGesture.state.rawValue == 1 {
+            print(emoji)
+            print(emoji123.text)
+            print("완료됐음")
+            audience.sendEmoji(sendEmoji: emoji, receiver: deviceName!)
+            
+        }
+    }
+    
+    func selectemoji(button: UIButton!, beforeButton: UIButton?) {
+        
+        if button.configuration?.baseBackgroundColor == UIColor(named: "secondaryColor") && beforeButton?.configuration?.baseBackgroundColor == UIColor(named: "primaryColor"){
+            button.configuration?.baseBackgroundColor = UIColor(named: "primaryColor")
+            beforeButton?.configuration?.baseBackgroundColor = UIColor(named: "secondaryColor")
+        } else if beforeButton == nil {
+            button.configuration?.baseBackgroundColor = UIColor(named: "primaryColor")
+        }
+        self.beforeButton = button
+    }
+    
+    func runHaptic(isHapticOn: Bool) {
         if isHapticOn {
             impactFeedbackGenerator = UIImpactFeedbackGenerator(style: .rigid)
             impactFeedbackGenerator?.impactOccurred()
@@ -98,3 +198,14 @@ class AudienceViewController: UIViewController {
     
     
 }
+
+//class MyLongPressGesture : UILongPressGestureRecognizer{
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent) {
+//        print("진동시작")
+//        // 진동 코드
+//    }
+//    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent) {
+//        print("진동끄읕")
+//        // 진동 종료
+//    }
+//}
