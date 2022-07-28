c532c7919d5bb5a91b7f5885e32d2b5b24e79bf5
Lawn <user.lawn@gmail.com>
2022-07-28 16:55:41 +0900

    [FIX] 불필요한 코드 제거

--- Pikipick/Pikipick/ButtonAnimView.swift
+++ Pikipick/Pikipick/ButtonAnimView.swift
@@ -70,83 +70,81 @@ struct ConfettiContainer: View {
     @Binding var finishedAnimationCouter: Int
     
     var body: some View{
-        ZStack{
-            Text(emoji.emoticon)
-                .offset(x: movement.x, y: movement.y)
-                .scaleEffect(movement.emojiSize)
-                .opacity(movement.opacity)
-                .onChange(of: animate) { _ in
-                    withAnimation(Animation.easeOut(duration: 0.4)) {
-                        emojiPositionSet.toggle()
-                        movement.opacity = 1
-                        movement.emojiSize = 20 * movement.emojiSize
-                        movement.x = UIScreen.screenHalfWidth * CGFloat.random(in: -0.4...0.4)
-                        movement.y = -UIScreen.screenHalfHeight * CGFloat.random(in: 0.4...0.8)
-                        withAnimation(.easeInOut(duration: 0.35)){
-                            animationValues[0] = true
-                        }
-                        withAnimation(.easeInOut(duration: 0.45).delay(0.06)){
-                            animationValues[1] = true
-                        }
-                        withAnimation(.easeInOut(duration: 0.35).delay(0.3)){
-                            animationValues[2] = true
-                        }
-                        withAnimation(.easeInOut(duration: 0.35).delay(0.4)){
-                            animationValues[3] = true
-                        }
-                        withAnimation(.easeInOut(duration: 0.55).delay(0.55)){
-                            animationValues[4] = true
-                        }
+        Text(emoji.emoticon)
+            .offset(x: movement.x, y: movement.y)
+            .scaleEffect(movement.emojiSize)
+            .opacity(movement.opacity)
+            .onChange(of: animate) { _ in
+                withAnimation(Animation.easeOut(duration: 0.4)) {
+                    emojiPositionSet.toggle()
+                    movement.opacity = 1
+                    movement.emojiSize = 20 * movement.emojiSize
+                    movement.x = UIScreen.screenHalfWidth * CGFloat.random(in: -0.4...0.4)
+                    movement.y = -UIScreen.screenHalfHeight * CGFloat.random(in: 0.4...0.8)
+                    withAnimation(.easeInOut(duration: 0.35)){
+                        animationValues[0] = true
                     }
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
-                        
-                        withAnimation(Animation.easeIn(duration: 3)) {
-                            movement.y = 200
-                            movement.opacity = 0.0
-                        }
+                    withAnimation(.easeInOut(duration: 0.45).delay(0.06)){
+                        animationValues[1] = true
+                    }
+                    withAnimation(.easeInOut(duration: 0.35).delay(0.3)){
+                        animationValues[2] = true
+                    }
+                    withAnimation(.easeInOut(duration: 0.35).delay(0.4)){
+                        animationValues[3] = true
+                    }
+                    withAnimation(.easeInOut(duration: 0.55).delay(0.55)){
+                        animationValues[4] = true
                     }
                 }
-                .overlay() {
-                    ZStack {
-                        ForEach(1...20,id: \.self){index in
-                            Circle()
-                                .fill(Color.primaryGradient)
-                                .frame(width: .random(in: 3...5), height: .random(in: 3...5))
-                                .offset(x: .random(in: -5...5), y: .random(in: -5...5))
-                                .offset(x: animationValues[3] ? 45 : 10)
-                                .rotationEffect(.init(degrees: Double(index) * 18.0))
-                                .scaleEffect(animationValues[2] ? 1 : 0.01)
-                                .opacity(animationValues[4] ? 0 : 1)
-                        }
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
+                    
+                    withAnimation(Animation.easeIn(duration: 3)) {
+                        movement.y = 200
+                        movement.opacity = 0.0
                     }
-                    .position(x: movement.x * 2 + 12, y: movement.y * 2 + 12)
-                }
-            
-        }
-        .onChange(of: emojiPositionSet) { _ in
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1){
-                withAnimation(.easeInOut(duration: 0.35)){
-                    animationValues[0] = true
-                }
-                withAnimation(.easeInOut(duration: 0.45).delay(0.06)){
-                    animationValues[1] = true
-                }
-                withAnimation(.easeInOut(duration: 0.35).delay(0.3)){
-                    animationValues[2] = true
-                }
-                withAnimation(.easeInOut(duration: 0.35).delay(0.4)){
-                    animationValues[3] = true
-                }
-                withAnimation(.easeInOut(duration: 0.55).delay(0.55)){
-                    animationValues[4] = true
                 }
             }
-        }
-        .onChange(of: animate) {_ in
-            DispatchQueue.main.asyncAfter(deadline: .now() + 3.5) {
-                finishedAnimationCouter += 1
+            .overlay() {
+                
+                ForEach(1...20,id: \.self){index in
+                    Circle()
+                        .fill(Color.primaryGradient)
+                        .frame(width: .random(in: 3...5), height: .random(in: 3...5))
+                        .offset(x: .random(in: -5...5), y: .random(in: -5...5))
+                        .offset(x: animationValues[3] ? 45 : 10)
+                        .rotationEffect(.init(degrees: Double(index) * 18.0))
+                        .scaleEffect(animationValues[2] ? 1 : 0.01)
+                        .opacity(animationValues[4] ? 0 : 1)
+                }
+                .position(x: movement.x * 2 + 12, y: movement.y * 2 + 12)
+                
                 
             }
-        }
+            .onChange(of: emojiPositionSet) { _ in
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.1){
+                    withAnimation(.easeInOut(duration: 0.35)){
+                        animationValues[0] = true
+                    }
+                    withAnimation(.easeInOut(duration: 0.45).delay(0.06)){
+                        animationValues[1] = true
+                    }
+                    withAnimation(.easeInOut(duration: 0.35).delay(0.3)){
+                        animationValues[2] = true
+                    }
+                    withAnimation(.easeInOut(duration: 0.35).delay(0.4)){
+                        animationValues[3] = true
+                    }
+                    withAnimation(.easeInOut(duration: 0.55).delay(0.55)){
+                        animationValues[4] = true
+                    }
+                }
+            }
+            .onChange(of: animate) {_ in
+                DispatchQueue.main.asyncAfter(deadline: .now() + 3.5) {
+                    finishedAnimationCouter += 1
+                    
+                }
+            }
     }
 }
